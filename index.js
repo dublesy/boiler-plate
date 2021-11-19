c8c25d173dcdb6a2c3514282e3e8af2980999810
박상용 <sangyong.park@monolith.co.kr>
2021-11-19 12:42:05 +0900

    올려요

--- index.js
+++ index.js
@@ -1,15 +1,34 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require("body-parser");
+const {User} = require("./model/User");
+const config = require('./config/key');
+
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+
+//applcation/json
+app.use(bodyParser.json());
 
 const moogoose = require('mongoose')
-moogoose.connect('mongodb+srv://dublesy:sypark!2@psy.8wpnq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+moogoose.connect(config.mongoURI, {
     useNewUrlParser : true, useUnifiedTopology: true
 }).then(() => console.log('MongoDB Connnected..'))
   .catch(err => console.log(err))
 
 
 
-app.get('/', (req, res) => res.send('Hello World'))
+app.get('/', (req, res) => res.send('Hello World 새해 복'))
 
+app.post('/register', (req, res) => {
+  // 회원 가입 할때 필요한 정보들을 Client에서 가져오면
+  // 그것들을 데이터 베이스에 넣어준다.
+  const user = new User(req.body)
+  
+  user.save((err,doc) => {
+    if(err) return res.json({ success : false, err})
+    return res.status(200).json({success : true})
+  })
+})
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
